e860a79a191037d52f8fe8cab0e49251a0cba01b
Raymond Truong <raymond.truong@xtremelabs.com>
2012-02-10 11:43:22 -0500

    added cucumber support files

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,15 @@ def title_test(header, suffix)
   it { should have_selector('h1',    text: header) }
   it { should have_selector('title', text: full_title(suffix)) }
 end
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.flash.error', text: message)
+  end
+end
