5183f2769b775566f02912835f87a3c5e84790b1
Raymond Truong <raymond.truong@xtremelabs.com>
2012-02-15 17:34:32 -0500

    added index page, more specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, :only => [:index, :edit, :update]
+  before_filter :correct_user,   :only => [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -18,4 +20,38 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  private
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end  
+  
+    def signed_in_user
+      store_location
+      redirect_to signin_path, :notice => "Please sign in." unless signed_in?
+    end
 end
