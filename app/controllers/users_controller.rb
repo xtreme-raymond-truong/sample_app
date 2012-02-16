429b7f44d1845dc966b192a5b691030de30e9f9d
Raymond Truong <raymond.truong@xtremelabs.com>
2012-02-16 13:13:55 -0500

    added deletion, pages, new tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, :only => [:index, :edit, :update]
+  before_filter :signed_in_user, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user,   :only => [:edit, :update]
+  before_filter :admin_user,     :only => :destroy
   
   def show
     @user = User.find(params[:id])
@@ -35,8 +36,14 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
   def index
-    @users = User.all
+    @users = User.paginate(:page => params[:page])
   end
 
   def show
@@ -53,5 +60,9 @@ class UsersController < ApplicationController
     def signed_in_user
       store_location
       redirect_to signin_path, :notice => "Please sign in." unless signed_in?
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
