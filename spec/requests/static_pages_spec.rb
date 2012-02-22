cd562609a1b6f473252fd751342bc40da8a3aa47
Raymond Truong <raymond.truong@xtremelabs.com>
2012-02-22 14:09:09 -0500

    finished 10.3.3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,22 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
     title_test('Sample App', full_title('Home'))
+    
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, :user => user, :content => "Lorem ipsum")
+        FactoryGirl.create(:micropost, :user => user, :content => "Dolor sit amet")
+        sign_in(user)
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("tr##{item.id}", :text => item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
