d2388b3418ae5236bb141b0ba0bcb4b7b190226a
Raymond Truong <raymond.truong@xtremelabs.com>
2012-02-22 10:05:08 -0500

    finished 10.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
   end
   
   def new
@@ -44,10 +45,6 @@ class UsersController < ApplicationController
   
   def index
     @users = User.paginate(:page => params[:page])
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
   
   private
