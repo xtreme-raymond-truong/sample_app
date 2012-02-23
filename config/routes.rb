4bd9b9d25549a1d60da90be58b1d850807225e6b
Raymond Truong <raymond.truong@xtremelabs.com>
2012-02-23 10:56:31 -0500

    completed 11.2.3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ SampleApp::Application.routes.draw do
   
   resources :sessions,   :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
- 
+  resources :relationships, :only => [:create, :destroy]
+  
   resources :users do
     member do
       get :following, :followers
